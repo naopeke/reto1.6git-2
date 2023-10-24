63f9ff9c09cd7a5350dbd5d392bd18eb20c656b2
naopeke <naopeke@protonmail.com>
2023-10-24 14:31:50 +0200

    commited .gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function suma(x, y){
+    return x + y;
+    };
+    
+    function resta(x, y){
+        return x - y;
+    }
+    
+    function producto(x, y){
+        return x * y;
+    }
+    
+    function division(x, y){
+        return x / y;
+    }
+
+    function cuadrado(x){
+        return x * x;
+    }
---
+++ run.js
@@ -0,0 +1,15 @@
+let myLib = require("./index");
+
+
+myLib.suma(3, 5);
+console.log(myLib.suma(3,5));
+
+myLib.resta(3, 5);
+console.log(myLib.resta(3,5));
+
+myLib.producto(3, 5);
+console.log(myLib.producto(3, 5));
+
+myLib.division(3, 5);
+console.log(myLib.division(3, 5));
+
